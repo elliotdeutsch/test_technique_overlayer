7bf824f9d15babe46e8a769b737b92d97e261eab
Elliot Deutsch <elliot.deutsch@gmail.com>
2025-05-14 14:11:46 +0200

    1st commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // // page d’accueil
+  // { path: 'home', component: HomeComponent },
+  // // fallback pour toute autre URL
+  // { path: '**', redirectTo: '', pathMatch: 'full' },
+];
